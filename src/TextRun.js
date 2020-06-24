d06c318cffc3e5cf1843b30c20b07d40095c28e0
Aaron Shafovaloff <ashafovaloff@instructure.com>
2020-06-23 22:40:06 -0600

    Font size

--- src/TextRun.js
+++ src/TextRun.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import OpenOfficeNode from './OpenOfficeNode';
 
+const stylistic = (node) => ['w:rPr'].includes(node.tagName);
+
+const extractStyles = (node) => {
+  const styles = {};
+  Array.from(node.childNodes).forEach((node) => {
+    switch (node.tagName) {
+      case 'w:sz':
+        styles.fontSize = `${node.getAttribute('w:val')}pt`;
+        break;
+      case 'szCs':
+        return;
+      default:
+        console.warn(`Did not parse tag ${node.tagName}`);
+        return;
+    }
+  });
+  return styles;
+};
+
 function TextRun({ node }) {
-  const children = Array.from(node.childNodes).map((node, i) => (
+  const childNodes = Array.from(node.childNodes);
+  const children = childNodes.map((node, i) => (
     <OpenOfficeNode key={i} node={node} />
   ));
-  const styles = {};
+  const styles = childNodes.filter(stylistic).reduce((styles, current) => {
+    return {
+      ...styles,
+      ...extractStyles(current),
+    };
+  }, {});
   return <span style={styles}>{children}</span>;
 }
 
